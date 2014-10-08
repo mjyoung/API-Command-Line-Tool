6e26a8272a4182ceb3ac21075d7b6d7ec5f3a263
Michael Young <michaeljy@gmail.com>
2014-10-08 16:02:01 -0500

    added documentation command placeholder

--- ibmwatson.js
+++ ibmwatson.js
@@ -6,6 +6,14 @@
 var program     = require('commander');
 var inquirer    = require('inquirer');
 var Table       = require('cli-table');
+
+/*
+    cli-table includes colors:
+        black, red, green, yellow, blue, magenta, cyan, white, gray, grey
+    background colors:
+        bgBlack, bgRed, bgGreen, bgYellow, bgBlue, bgMagenta, bgCyan, bgWhite
+*/
+
 var Chance      = require('chance');
 var ProgressBar = require('progress');
 
@@ -82,94 +90,110 @@ program
     }
     console.log('');
 
-    console.log("Hi, I'm Watson! I'm going to walk you through setting up your new project with the Watson APIs!");
+    console.log("Hi, I'm Watson! I'm going to walk you through setting up your new project with the Watson APIs! \n".blue);
 
-    // var questions = [
-    //   {
-    //     type: "confirm",
-    //     name: "toBeDelivered",
-    //     message: "Is it for a delivery",
-    //     default: false
-    //   },
-    //   {
-    //     type: "input",
-    //     name: "phone",
-    //     message: "What's your phone number",
-    //     validate: function( value ) {
-    //       var pass = value.match(/^([01]{1})?[\-\.\s]?\(?(\d{3})\)?[\-\.\s]?(\d{3})[\-\.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i);
-    //       if (pass) {
-    //         return true;
-    //       } else {
-    //         return "Please enter a valid phone number";
-    //       }
-    //     }
-    //   },
-    //   {
-    //     type: "list",
-    //     name: "size",
-    //     message: "What size do you need",
-    //     choices: [ "Large", "Medium", "Small" ],
-    //     filter: function( val ) { return val.toLowerCase(); }
-    //   },
-    //   {
-    //     type: "input",
-    //     name: "quantity",
-    //     message: "How many do you need",
-    //     validate: function( value ) {
-    //       var valid = !isNaN(parseFloat(value));
-    //       return valid || "Please enter a number";
-    //     },
-    //     filter: Number
-    //   },
-    //   {
-    //     type: "expand",
-    //     name: "toppings",
-    //     message: "What about the toping",
-    //     choices: [
-    //       {
-    //         key: "p",
-    //         name: "Peperonni and chesse",
-    //         value: "PeperonniChesse"
-    //       },
-    //       {
-    //         key: "a",
-    //         name: "All dressed",
-    //         value: "alldressed"
-    //       },
-    //       {
-    //         key: "w",
-    //         name: "Hawaïan",
-    //         value: "hawaian"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     type: "rawlist",
-    //     name: "beverage",
-    //     message: "You also get a free 2L beverage",
-    //     choices: [ "Pepsi", "7up", "Coke" ]
-    //   },
-    //   {
-    //     type: "input",
-    //     name: "comments",
-    //     message: "Any comments on your purchase experience",
-    //     default: "Nope, all good!"
-    //   },
-    //   {
-    //     type: "list",
-    //     name: "prize",
-    //     message: "For leaving a comments, you get a freebie",
-    //     choices: [ "cake", "fries" ],
-    //     when: function( answers ) {
-    //       return answers.comments !== "Nope, all good!";
-    //     }
-    //   }
-    // ];
+    var questions = [
+      {
+        type: "input",
+        name: "username",
+        message: "What is your Bluemix username?"
+      },
+      {
+        type: "password",
+        message: "Please enter your Bluemix password:",
+        name: "password"
+      },
+      {
+        type: "input",
+        name: "project",
+        message: "What would you like to name your project?"
+      }
 
-    // inquirer.prompt( questions, function( answers ) {
-    //   console.log("\nOrder receipt:");
-    //   console.log( JSON.stringify(answers, null, "  ") );
-    // });
+      // {
+      //   type: "confirm",
+      //   name: "toBeDelivered",
+      //   message: "Is it for a delivery",
+      //   default: false
+      // },
+      // {
+      //   type: "input",
+      //   name: "phone",
+      //   message: "What's your phone number",
+      //   validate: function( value ) {
+      //     var pass = value.match(/^([01]{1})?[\-\.\s]?\(?(\d{3})\)?[\-\.\s]?(\d{3})[\-\.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i);
+      //     if (pass) {
+      //       return true;
+      //     } else {
+      //       return "Please enter a valid phone number";
+      //     }
+      //   }
+      // },
+      // {
+      //   type: "list",
+      //   name: "size",
+      //   message: "What size do you need",
+      //   choices: [ "Large", "Medium", "Small" ],
+      //   filter: function( val ) { return val.toLowerCase(); }
+      // },
+      // {
+      //   type: "input",
+      //   name: "quantity",
+      //   message: "How many do you need",
+      //   validate: function( value ) {
+      //     var valid = !isNaN(parseFloat(value));
+      //     return valid || "Please enter a number";
+      //   },
+      //   filter: Number
+      // },
+      // {
+      //   type: "expand",
+      //   name: "toppings",
+      //   message: "What about the toping",
+      //   choices: [
+      //     {
+      //       key: "p",
+      //       name: "Peperonni and chesse",
+      //       value: "PeperonniChesse"
+      //     },
+      //     {
+      //       key: "a",
+      //       name: "All dressed",
+      //       value: "alldressed"
+      //     },
+      //     {
+      //       key: "w",
+      //       name: "Hawaïan",
+      //       value: "hawaian"
+      //     }
+      //   ]
+      // },
+      // {
+      //   type: "rawlist",
+      //   name: "beverage",
+      //   message: "You also get a free 2L beverage",
+      //   choices: [ "Pepsi", "7up", "Coke" ]
+      // },
+      // {
+      //   type: "input",
+      //   name: "comments",
+      //   message: "Any comments on your purchase experience",
+      //   default: "Nope, all good!"
+      // },
+      // {
+      //   type: "list",
+      //   name: "prize",
+      //   message: "For leaving a comments, you get a freebie",
+      //   choices: [ "cake", "fries" ],
+      //   when: function( answers ) {
+      //     return answers.comments !== "Nope, all good!";
+      //   }
+      // }
+    ];
+
+    inquirer.prompt( questions, function( answers ) {
+      console.log("Answers");
+      console.log( JSON.stringify(answers, null, "  ") );
+    });
   });
 
 program
@@ -179,6 +203,12 @@ program
     console.log(service_all_table.toString());
   });
 
+program
+  .command("docs")
+  .description("Browse the Watson API Documentation.")
+  .action (function() {
+    console.log('Documentation!');
+  });
 
 program.parse(process.argv);
 
