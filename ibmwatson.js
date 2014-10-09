baee50b3ae9bc3406f48e51e94c7e6c730b90baa
Michael Young <michaeljy@gmail.com>
2014-10-09 16:20:10 -0500

    login stuff not working

--- ibmwatson.js
+++ ibmwatson.js
@@ -59,6 +59,10 @@ var watsonLogo = [
 "            .:/+oss+++oo++:."
 ];
 
+/*
+  ===[ GLOBAL VARIABLES ]===
+*/
+var loggedIn = true;
 
 /*
     ===[ TABLES START ]===
@@ -127,6 +131,7 @@ program
     ];
 
     inquirer.prompt( questions, function( answers ) {
+      loggedIn = true;
       console.log("You have successfully logged in.");
       // console.log( JSON.stringify(answers, null, "  ") );
     });
@@ -136,154 +141,159 @@ program
   .command("new")
   .description("Create a new Watson project, which will house your individual API instances.")
   .action (function() {
-
-    console.log('');
-    for (var i = 0; i < watsonLogo.length; i++) {
-      console.log(watsonLogo[i]);
+    if (!loggedIn) {
+      console.log("You need to log in to Bluemix to access that command.".blue);
+      console.log("Please type ".blue + "ibmwatson login ".yellow + "to login to Bluemix.".blue);
     }
-    console.log('');
+    else {
+      console.log('');
+      for (var i = 0; i < watsonLogo.length; i++) {
+        console.log(watsonLogo[i]);
+      }
+      console.log('');
 
-    console.log("Hi, I'm Watson! I'm going to walk you through setting up your new project with the Watson APIs! \n".blue);
+      console.log("Hi, I'm Watson! I'm going to walk you through setting up your new project with the Watson APIs! \n".blue);
 
-    var questions = [
-      {
-        type: "input",
-        name: "project",
-        message: "What would you like to name your project?"
-      },
-      {
-        type: "checkbox",
-        message: "Which APIs would you like to use with your project?",
-        name: "selectedAPIs",
-        choices: [
-          // new inquirer.Separator("These are the most popular APIs among the Watson Developer community:"),
-          // new inquirer.Separator("And these are the rest:"),
-          {
-            name: "Concept Expansion",
-          },
-          {
-            name: "Language Identification"
-          },
-          {
-            name: "Machine Translation",
-          },
-          {
-            name: "Message Resonance",
-            // checked: true
-          },
-          {
-            name: "Question and Answer",
-            // disabled: "temporarily unavailable"
-          },
-          {
-            name: "Relationship Extraction",
-          },
-          {
-            name: "User Modeling"
-          },
-          {
-            name: "Visualization Rendering",
+      var questions = [
+        {
+          type: "input",
+          name: "project",
+          message: "What would you like to name your project?"
+        },
+        {
+          type: "checkbox",
+          message: "Which APIs would you like to use with your project?",
+          name: "selectedAPIs",
+          choices: [
+            // new inquirer.Separator("These are the most popular APIs among the Watson Developer community:"),
+            // new inquirer.Separator("And these are the rest:"),
+            {
+              name: "Concept Expansion",
+            },
+            {
+              name: "Language Identification"
+            },
+            {
+              name: "Machine Translation",
+            },
+            {
+              name: "Message Resonance",
+              // checked: true
+            },
+            {
+              name: "Question and Answer",
+              // disabled: "temporarily unavailable"
+            },
+            {
+              name: "Relationship Extraction",
+            },
+            {
+              name: "User Modeling"
+            },
+            {
+              name: "Visualization Rendering",
+            }
+          ],
+          validate: function( answer ) {
+            if ( answer.length < 1 ) {
+              return "You must choose at least one service.";
+            }
+            return true;
           }
-        ],
-        validate: function( answer ) {
-          if ( answer.length < 1 ) {
-            return "You must choose at least one service.";
-          }
-          return true;
         }
-      }
-    ];
-
-    inquirer.prompt( questions, function( answers ) {
-      // console.log("Answers");
-      // console.log( JSON.stringify(answers, null, "  ") );
-
-      /*
-          ===[ LOADING TEXT ]===
-      */
-      var loader = [
-        "I'm working",
-        "I'm working.",
-        "I'm working..",
-        "I'm working..."
       ];
-      /*
-          ===[ LOADING TEXT END ]===
-      */
 
-      var newBottomBar = function() {
-        var ui = new inquirer.ui.BottomBar({ bottomBar: loader[i % 4] });
-        return ui;
-      }
+      inquirer.prompt( questions, function( answers ) {
+        // console.log("Answers");
+        // console.log( JSON.stringify(answers, null, "  ") );
 
-      var createApplication = function() {
-        console.log("Creating your application in Bluemix...".yellow);
-        var ui = newBottomBar();
-        var i = 4;
+        /*
+            ===[ LOADING TEXT ]===
+        */
+        var loader = [
+          "I'm working",
+          "I'm working.",
+          "I'm working..",
+          "I'm working..."
+        ];
+        /*
+            ===[ LOADING TEXT END ]===
+        */
 
-        var count = 1;
-        var interval = setInterval(function() {
-          ui.updateBottomBar( loader[i++ % 4] );
-          count++;
-          if (count == 25) {
-            ui.updateBottomBar("I'm done creating your application!\n".green);
-            console.log("  Application ".green + ">> " + "Services ".yellow + ">> " + "Boilerplate ".yellow);
-            clearInterval(interval);
-            createServices();
-          }
-        }, 200 );
+        var newBottomBar = function() {
+          var ui = new inquirer.ui.BottomBar({ bottomBar: loader[i % 4] });
+          return ui;
+        }
 
-      };
+        var createApplication = function() {
+          console.log("Creating your application in Bluemix...".yellow);
+          var ui = newBottomBar();
+          var i = 4;
 
-      var createServices = function() {
-        console.log("Creating your services in Bluemix...".yellow);
-        var ui = newBottomBar();
-        var i = 4;
+          var count = 1;
+          var interval = setInterval(function() {
+            ui.updateBottomBar( loader[i++ % 4] );
+            count++;
+            if (count == 25) {
+              ui.updateBottomBar("I'm done creating your application!\n".green);
+              console.log("  Application ".green + ">> " + "Services ".yellow + ">> " + "Boilerplate ".yellow);
+              clearInterval(interval);
+              createServices();
+            }
+          }, 200 );
 
-        var count = 1;
-        var interval = setInterval(function() {
-          ui.updateBottomBar( loader[i++ % 4] );
-          count++;
-          if (count == 25) {
-            ui.updateBottomBar("I'm done creating your services!\n".green);
-            console.log("  Application ".green + ">> " + "Services ".green + ">> " + "Boilerplate ".yellow);
-            clearInterval(interval);
-            createBoilerplate();
-          }
-        }, 200 );
+        };
 
-      };
+        var createServices = function() {
+          console.log("Creating your services in Bluemix...".yellow);
+          var ui = newBottomBar();
+          var i = 4;
 
-      // var cmd = spawn(cmdify("ls"), [ "-la" ], { stdio: "pipe" });
-      // cmd.stdout.pipe( ui.log );
-      // cmd.on( "close", function() {
-      //   ui.updateBottomBar("I'm done working!\n");
-      //   process.exit();
-      // });
+          var count = 1;
+          var interval = setInterval(function() {
+            ui.updateBottomBar( loader[i++ % 4] );
+            count++;
+            if (count == 25) {
+              ui.updateBottomBar("I'm done creating your services!\n".green);
+              console.log("  Application ".green + ">> " + "Services ".green + ">> " + "Boilerplate ".yellow);
+              clearInterval(interval);
+              createBoilerplate();
+            }
+          }, 200 );
 
-      var createBoilerplate = function() {
-        console.log("Creating boilerplate code for your project...".yellow);
-        var ui = newBottomBar();
-        var i = 4;
+        };
 
-        var count = 1;
-        var interval = setInterval(function() {
-          ui.updateBottomBar( loader[i++ % 4] );
-          count++;
-          if (count == 25) {
-            ui.updateBottomBar("I'm done creating your boilerplatecode!\n".green);
-            console.log("  Application ".green + ">> " + "Services ".green + ">> " + "Boilerplate ".green);
-            clearInterval(interval);
-            console.log("Type ".blue + "cf push ".yellow + answers.project.yellow + " to test your application in Bluemix.".blue);
-            process.exit();
-          }
-        }, 200 );
+        // var cmd = spawn(cmdify("ls"), [ "-la" ], { stdio: "pipe" });
+        // cmd.stdout.pipe( ui.log );
+        // cmd.on( "close", function() {
+        //   ui.updateBottomBar("I'm done working!\n");
+        //   process.exit();
+        // });
 
-      };
+        var createBoilerplate = function() {
+          console.log("Creating boilerplate code for your project...".yellow);
+          var ui = newBottomBar();
+          var i = 4;
 
-      createApplication();
+          var count = 1;
+          var interval = setInterval(function() {
+            ui.updateBottomBar( loader[i++ % 4] );
+            count++;
+            if (count == 25) {
+              ui.updateBottomBar("I'm done creating your boilerplatecode!\n".green);
+              console.log("  Application ".green + ">> " + "Services ".green + ">> " + "Boilerplate ".green);
+              clearInterval(interval);
+              console.log("Type ".blue + "cf push ".yellow + answers.project.yellow + " to test your application in Bluemix.".blue);
+              process.exit();
+            }
+          }, 200 );
 
-    });
+        };
+
+        createApplication();
+
+      });
+    }
   });
 
 program
